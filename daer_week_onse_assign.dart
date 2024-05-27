e0e2e1a11503a1c39f89a2eb396d12591c7b0a6e
christopher639 <bundichristopher639@gmail.com>
2024-05-27 11:34:09 +0300

    first commit

---
+++ daer_week_onse_assign.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Integer data type (int)
+  int myAge = 22;
+
+  // Double data type (double)
+  double myHeight = 5.61;
+
+  // String data type (String
+  String myName = "Christopher Bundi";
+
+  // List data type (List)
+  List<String> plpSoftware_developement_course = ["Dart with Flutter", "databases ",
+  "Web Technlogies","Enterpreneurship", "Python","Software engeneering"];
+
+  // Map data type (Map)
+  Map<String, dynamic> myInfo = {
+    'name': 'John Doe',
+    'age': 25,
+    'height': 5.11,
+    'hobbies': ['reading', 'gardening', 'swimming']
+  };
+
+  // Displaying values
+  print("My age: $myAge");
+  print("My height: $myHeight");
+  print("My name: $myName");
+  print("My Power learn courses am taking are: $plpSoftware_developement_course");
+  print("My info: $myInfo");
+}
